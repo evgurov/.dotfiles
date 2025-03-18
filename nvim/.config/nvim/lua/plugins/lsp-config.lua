c435c96f0d4dd2d4bfcb4ecef192497210e6499e
evgurov <evgurovv@yandex.ru>
2025-03-19 00:51:47 +0300

    blink.cmp added

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -10,20 +10,22 @@ return {
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = {
-                    -- Lua --
-                    "lua_ls",
-                    -- Python --
-                    "basedpyright",
-                    "ruff" 
-                },
+					-- Lua --
+					"lua_ls",
+					-- Python --
+					"basedpyright",
+					"ruff",
+				},
 			})
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
+
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
 			lspconfig.basedpyright.setup({
 				settings = {
 					basedpyright = {
@@ -32,8 +34,10 @@ return {
 						},
 					},
 				},
+				capabilities = capabilities,
 			})
-            lspconfig.ruff.setup({})
+			lspconfig.ruff.setup({ capabilities = capabilities })
+
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
