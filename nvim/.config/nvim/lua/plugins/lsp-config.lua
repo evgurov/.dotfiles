8c93721b76882c2cc04283a248bb48079f483e04
evgurov <evgurovv@yandex.ru>
2025-03-18 18:34:57 +0300

    none-ls added, all lua files formatted

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,36 +1,36 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls", "basedpyright"}
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.basedpyright.setup({
-                settings = {
-                    basedpyright = {
-                        analysis = {
-                            typeCheckingMode = 'off'
-                        }
-                    }
-                }
-            })
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
-            vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
-            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "basedpyright" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.basedpyright.setup({
+				settings = {
+					basedpyright = {
+						analysis = {
+							typeCheckingMode = "off",
+						},
+					},
+				},
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -1,14 +1,13 @@
 -- Set tab behavior
-vim.opt.tabstop = 4        -- Display tab as 4 spaces
-vim.opt.shiftwidth = 4     -- Indentation level
-vim.opt.softtabstop = 4    -- Space count when pressing Tab
-vim.opt.expandtab = true   -- Convert tabs to spaces
-vim.g.mapleader = ' '      -- Set leader
+vim.opt.tabstop = 4 -- Display tab as 4 spaces
+vim.opt.shiftwidth = 4 -- Indentation level
+vim.opt.softtabstop = 4 -- Space count when pressing Tab
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.g.mapleader = " " -- Set leader
 
 -- Set line numbers
-vim.opt.number = true      -- Enable line numbers
+vim.opt.number = true -- Enable line numbers
 vim.opt.relativenumber = true -- Show relative line numbers
 
 -- Set rulers
 vim.opt.colorcolumn = "100"
-
