decd126767ca0dbc7544b8dc56c239bce57e2e8e
evgurov <evgurovv@yandex.ru>
2025-06-17 00:56:28 +0300

    lsp-config.lua fixed

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -19,30 +19,116 @@ return {
 			})
 		end,
 	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local blink = require("blink.cmp")
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig   = require("lspconfig")
+            local blink       = require("blink.cmp")
+            local capabilities = blink.get_lsp_capabilities({}, false)
 
-			local capabilities = blink.get_lsp_capabilities({}, false)
+            ------------------------------------------------------------------------
+            -- Make diagnostics pretty
+            ------------------------------------------------------------------------
 
-			lspconfig.lua_ls.setup({capabilities = capabilities})
-			lspconfig.basedpyright.setup({
-				settings = {
-					basedpyright = {
-						analysis = {
-							typeCheckingMode = "off",
-						},
-					},
-				},
-			})
-			lspconfig.ruff.setup({})
+            vim.diagnostic.config({
+                virtual_text = true,
+                underline = false,
+            })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            ------------------------------------------------------------------------
+            -- 1. Helper: generic on_attach that adds your key-maps once
+            ------------------------------------------------------------------------
+            local function on_attach_keys(_, bufnr)
+                local map = function(keys, fn, desc)
+                    vim.keymap.set("n", keys, fn, { buffer = bufnr, desc = desc })
+                end
+
+                map("K",          vim.lsp.buf.hover,        "Hover")
+                map("<leader>gd", vim.lsp.buf.definition,   "Go to definition")
+                map("<leader>gr", vim.lsp.buf.references,   "References")
+                map("<leader>ca", vim.lsp.buf.code_action,  "Code action")
+                -- manual formatter: <leader>cf
+                map("<leader>cf", function()
+                    vim.lsp.buf.format({ timeout_ms = 2000 })
+                end, "Format (Ruff)")
+                map("<leader>d", function()
+                    vim.diagnostic.open_float()
+                end, "Show diagnostic")
+            end
+
+            ------------------------------------------------------------------------
+            -- 2. Lua LS (unchanged, but reuse on_attach_keys)
+            ------------------------------------------------------------------------
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                on_attach    = on_attach_keys,
+            })
+
+            ------------------------------------------------------------------------
+            -- 3. basedpyright – turn OFF formatting & code-actions so Ruff can own them
+            ------------------------------------------------------------------------
+            lspconfig.basedpyright.setup({
+                capabilities = capabilities,
+                settings = {
+                    basedpyright = {
+                        analysis = {
+                            typeCheckingMode = "off",
+                        },
+                        disableOrganizeImports = true,
+                    },
+                },
+                on_attach = function(client, bufnr)
+                    client.server_capabilities.documentFormattingProvider = false
+                    client.server_capabilities.codeActionProvider         = false
+                    on_attach_keys(client, bufnr)
+                end,
+            })
+
+            ------------------------------------------------------------------------
+            -- 4. Ruff – lint + format + organise-imports, but NO hover/completion
+            ------------------------------------------------------------------------
+            lspconfig.ruff.setup({
+                capabilities = capabilities,
+                init_options = {
+                    settings = {
+                        format          = { enable = true },
+                        lint            = { enable = true, select = { "E", "F", "I" } },
+                        organizeImports = true,
+                    },
+                },
+                on_attach = function(client, bufnr)
+                    client.server_capabilities.hoverProvider      = false
+                    client.server_capabilities.completionProvider = nil
+                    on_attach_keys(client, bufnr)
+                end,
+            })
+        end,
+    },
+    -- {
+	-- 	"neovim/nvim-lspconfig",
+	-- 	config = function()
+	-- 		local lspconfig = require("lspconfig")
+	-- 		local blink = require("blink.cmp")
+
+	-- 		local capabilities = blink.get_lsp_capabilities({}, false)
+
+	-- 		lspconfig.lua_ls.setup({capabilities = capabilities})
+	-- 		lspconfig.basedpyright.setup({
+	-- 			settings = {
+	-- 				basedpyright = {
+	-- 					analysis = {
+	-- 						typeCheckingMode = "off",
+	-- 					},
+    --                     disableOrganizeImports = true,
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 		lspconfig.ruff.setup({})
+
+	-- 		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+	-- 		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+	-- 		vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+	-- 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+	-- 	end,
+	-- },
 }
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -3,7 +3,9 @@ vim.opt.tabstop = 4 -- Display tab as 4 spaces
 vim.opt.shiftwidth = 4 -- Indentation level
 vim.opt.softtabstop = 4 -- Space count when pressing Tab
 vim.opt.expandtab = true -- Convert tabs to spaces
+-- Remaps
 vim.g.mapleader = " " -- Set leader
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 
 -- Set line numbers
 vim.opt.number = true -- Enable line numbers
@@ -11,3 +13,6 @@ vim.opt.relativenumber = true -- Show relative line numbers
 
 -- Set rulers
 vim.opt.colorcolumn = "100"
+
+-- Set clipboard to copy and paste
+vim.opt.clipboard="unnamedplus"
